b478c16f4ce780c276a9b19e4e2e5507368be4c1
md-liton <mdliton5506@gmail.com>
2023-08-10 23:37:09 +0600

    STRICT

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// $(document).ready(function(){
+//     $('.simple_desing_item').slick({
+//         infinite: true,
+//         slidesToShow: 3,
+//         slidesToScroll: 3
+//       });
+//   })
